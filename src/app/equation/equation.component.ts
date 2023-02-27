8f8ddf9bc563a1949d1b2ff8beb0cebe24031525
anubhav <anubhavkoul01@gmail.com>
2023-02-27 11:19:51 +0530

    Time taken in seconds per solution shown in the UI
    Time taken in seconds per solution is calculated using simple typescript code in the equation component class. This code will later be updated to use RxJS instead of just typescript.

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { MathValidators } from '../math-validators';
+import { delay, filter } from 'rxjs/operators'
 
 @Component({
   selector: 'app-equation',
@@ -7,23 +9,40 @@ import { FormGroup, FormControl, AbstractControl } from '@angular/forms';
   styleUrls: ['./equation.component.css']
 })
 export class EquationComponent implements OnInit {
+
+  secondsPerSolution = 0;
   mathForm = new FormGroup({
     a: new FormControl(this.randomNumber()),
     b: new FormControl(this.randomNumber()),
     answer: new FormControl(''),
   }, [
-    (form: AbstractControl) => {
-      if (parseInt(form.value.answer) == form.value.a + form.value.b){
-        return null;
-      }
-      return { addition: true};
-    }
+    MathValidators.addition('answer','a','b'),
+    Validators.required,
   ]);
 
 
   constructor() { }
 
   ngOnInit(): void {
+
+    const startTime = new Date();
+    let numberSolved = 0;
+
+    this.mathForm.statusChanges.pipe(
+      filter(value => value === 'VALID'),
+      delay(100)
+      ).subscribe(() => {
+        numberSolved++;
+        this.secondsPerSolution = (
+          new Date().getTime() - startTime.getTime()
+        ) / numberSolved / 1000;
+        this.mathForm.patchValue({
+          a: this.randomNumber(),
+          b: this.randomNumber(),
+          answer: ''
+        })
+
+    })
   }
 
   randomNumber(){
